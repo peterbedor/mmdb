fcbf3e2488f1c06ac54fcb2cde57abfc9daeee6a
Peter Bedor Jr <peter@caddis.co>
2016-04-09 23:29:29 -0500

    Add class to config

--- config/app.php
+++ config/app.php
@@ -148,6 +148,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         TwigBridge\ServiceProvider::class,
+		Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Application Service Providers...
