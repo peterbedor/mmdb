16a18ffb3711ad42cc11bd3694f5d30084970bb1
Peter Bedor Jr <peter@caddis.co>
2016-04-11 00:39:15 -0500

    Add static method

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -7,12 +7,30 @@ use \GuzzleHttp\Client as Guzzle;
 
 class MovieController extends Controller
 {
+	public static $cast;
+	public static $movie;
+
 	public static function getPopularTitles()
 	{
 		$movies = (new Guzzle())->get('http://api.themoviedb.org/3/movie/popular?api_key=' . env('API_KEY'));
 
 		$response = json_decode($movies->getBody());
 
-		return $response->results;
+		if (count($response)) {
+			return $response->results;
+		}
+
+		return false;
+	}
+
+	public static function getMovie($id)
+	{
+		$movie = (new Guzzle())->get('http://api.themoviedb.org/3/movie/' . $id . '?api_key=' . env('API_KEY'));
+
+		if ($movie) {
+			return json_decode($movie->getBody());
+		}
+
+		return false;
 	}
 }
