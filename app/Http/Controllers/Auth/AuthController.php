507a28be1d51fe565ee3c782a95b92cc7219a58e
Peter Bedor Jr <peter@caddis.co>
2016-04-24 00:00:09 -0500

    Fix redirect path

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/collection';
 
     /**
      * Create a new authentication controller instance.
