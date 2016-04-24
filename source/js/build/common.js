52208315dd612ffadfd1cac5c6ef030991217dba
Peter Bedor Jr <peter@caddis.co>
2016-04-23 23:58:19 -0500

    Add common

--- source/js/build/common.js
+++ source/js/build/common.js
@@ -1,11 +1,69 @@
 Wee.fn.make('common', {
 	init: function() {
-		this.$private.mobileNav();
+		var scope = this;
+
+		scope.$private.mobileNav();
+		scope.$private.searchToggle();
+
+		scope.$private.extend();
 	}
 }, {
 	mobileNav: function() {
-		$('ref:mobileNavToggle').on('click', function() {
-			$('ref:mobileNav').toggleClass('-is-active');
+		var $nav = $('ref:mainNav'),
+			$body = $(Wee._body),
+			isActive = '-is-active',
+			isDisabled = '-is-disabled';
+
+		Wee.events.on({
+			'ref:mobileNavToggle': {
+				click: function() {
+					$nav.show();
+					$body.addClass(isDisabled);
+
+					setTimeout(function() {
+						$nav.addClass(isActive);
+					}, 50);
+				}
+			},
+			'ref:mobileNavClose': {
+				click: function() {
+					$nav.removeClass(isActive);
+					$body.addClass(isDisabled);
+
+					setTimeout(function() {
+						$nav.hide();
+					}, 50);
+				}
+			}
+		});
+	},
+	searchToggle: function() {
+		var $search = $('ref:headerSearch'),
+			$add = $('ref:headerAdd'),
+			isActive = '-is-active';
+
+		Wee.events.on({
+			'ref:searchMovie': {
+				click: function() {
+					var isHidden = '-is-hidden';
+
+					$search.toggleClass(isActive);
+					$(this).toggleClass(isActive);
+					$('ref:headerHeading').toggleClass(isHidden);
+					$('ref:mobileNavToggle').toggleClass(isHidden);
+
+					if ($add.hasClass(isActive)) {
+						$add.removeClass(isActive);
+					}
+				}
+			}
+		});
+	},
+	extend: function() {
+		Wee.fn.extend({
+			click: function(fn) {
+				$(this).on('click', fn);
+			}
 		});
 	}
 });
